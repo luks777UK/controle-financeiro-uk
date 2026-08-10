0b3481c86262a6a49f60420619783dcd441915bf
luks777UK <312508428+luks777UK@users.noreply.github.com>
2026-08-10 12:01:37 +0100

    v7.0.0-beta.1 - Financial Intelligence

--- sw.js
+++ sw.js
@@ -1,2 +1,1 @@
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',event=>event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));await self.registration.unregister();await self.clients.claim();})()));
+self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.registration.unregister();await self.clients.claim()})()));
